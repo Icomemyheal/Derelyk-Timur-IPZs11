5a00c79aa2de6b4f3ad91a87ce312ed816fb30bf
Timur Derelyk <derelyk2004@gmail.com>
2023-09-11 21:29:08 +0300

    add features section (mobile)

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const burgerBtn = document.querySelector('.header__btn--menu');
 const headerMenu = document.querySelector('.header__menu');
 const body = document.getElementsByTagName('body');
-// const html = document.getElementsByTagName('html');
 
 burgerBtn.addEventListener('click', (e) => {
     e.preventDefault();
     burgerBtn.classList.toggle('active');
     headerMenu.classList.toggle('open');
     body[0].classList.toggle('fixing');
-    // html[0].classList.toggle('fixing');
 });
