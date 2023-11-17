e41af0da71a38cd93e9549bc345facb460240517
Timur Derelyk <derelyk2004@gmail.com>
2023-11-17 18:28:53 +0200

    final version

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-//Utils
-const body = document.getElementsByTagName('body');
-let min768px = window.matchMedia('(min-width: 768px)').matches;
-let min1200px = window.matchMedia('(min-width: 1200px)').matches;
 //BurgerMenu
 const burgerBtn = document.querySelector('.js-header__btn--menu');
 const headerMenu = document.querySelector('.js-header__menu');
@@ -18,50 +14,6 @@ const itemFixApps = document.getElementById('fix-apps');
 //Changing Modal Svg
 const modalFormInputs = document.querySelectorAll('.js-modal-input');
 const modalFormSvgs = document.querySelectorAll('.js-icon');
-//Marging Elements
-window.addEventListener('resize', () => {
-    location.reload();
-});
-//Adaptive Load Fix
-document.addEventListener('DOMContentLoaded', () => {
-    const teamItems = document.querySelectorAll('.team__cards--item');
-    const featuresItems = document.querySelectorAll('.features__li--item');
-    const specializationItems = document.querySelectorAll('.specialization__li--item');
-    const clientsItems = document.querySelectorAll('.clients__li--item');
-    const projectsItems = document.querySelectorAll('.projects__li--item');
-
-    for(let i = 0; i < teamItems.length; i++){
-        if(i % 4 === 0 && min1200px){
-            teamItems[i].classList.add('no-left-margin');
-        }
-    }
-    for(let i = 0; i < featuresItems.length; i++){
-        if(i % 4 === 0 && min1200px){
-            featuresItems[i].classList.add('no-left-margin');
-        }
-    }
-    for(let i = 0; i < specializationItems.length; i++){
-        if(i % 3 === 0 && min1200px){
-            specializationItems[i].classList.add('no-left-margin');
-        }
-    }
-    for(let i = 0; i < clientsItems.length; i++){
-        if(i % 3 === 0 && min768px && !min1200px){
-            clientsItems[i].classList.add('no-left-margin');
-        }
-        if(i % 6 === 0 && min1200px){
-            clientsItems[i].classList.add('no-left-margin');
-        }
-    }
-    for(let i = 0; i < projectsItems.length; i++){
-        if(i % 2 === 0 && min768px && !min1200px){
-            projectsItems[i].classList.add('no-left-margin');
-        }
-        if(i % 3 === 0 && min1200px){
-            projectsItems[i].classList.add('no-left-margin');
-        }
-    }
-});
 
 modalFormInputs.forEach((input, index) => {
     input.addEventListener('focus', () => {
@@ -77,46 +29,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const spanCheckbox = document.querySelector('.js-span-checkbox');
     const modalFormCheckbox = document.querySelector('.js-modal-checkbox');
 
-spanCheckbox.addEventListener('click', () => {
+spanCheckbox?.addEventListener('click', () => {
     modalFormCheckbox.checked = !modalFormCheckbox.checked;
 });
 });
-//Active page
-document.addEventListener('DOMContentLoaded', () => {
-    const currentPage = location.pathname;
-    const navLinks = document.querySelectorAll('.header__nav--link');
 
-const navLink = navLinks.forEach((item) => {
-    let linkPage = '/Derelyk-Timur-IPZs11/' + item.getAttribute('href');
-    item.classList.remove('active');
-    if(currentPage == linkPage){
-        item.parentElement.classList.add('active');
-    } else if(currentPage == '/Derelyk-Timur-IPZs11/' || currentPage == './'){
-        navLinks[0].parentElement.classList.add('active');
-    } else {
-        item.parentElement.classList.remove('active');
-    }
-});
-});
 const filteredBtn = filtersBtns.forEach((btn) => {
+    
     btn.addEventListener(('click'), () => {
-        filtersBtns.forEach(item => item.classList.remove('active'));
+        filtersBtns.forEach(e => {e.classList.remove('active');});
         btn.classList.add('active');
-        if(btn.classList.contains('active') && btn.dataset.type == 'design' && min768px){
-            itemFixDesign.style.setProperty('margin-left', '30px', 'important');
-        } else {
-            itemFixDesign.style.setProperty('margin-left', '0px', 'important');
-        }
-        if(btn.classList.contains('active') && btn.dataset.type == 'apps' && min768px){
-            itemFixApps.style.setProperty('margin-left', '30px', 'important');
-        } else {
-            itemFixApps.style.setProperty('margin-left', '0px', 'important');
-        }
-        if(btn.classList.contains('active') && btn.dataset.type == 'apps' && min1200px){
-            itemFixApps.style.setProperty('margin-left', '30px', 'important');
-        } else if(btn.classList.contains('active') && btn.dataset.type !== 'apps' && min1200px){
-            itemFixApps.style.setProperty('margin-left', '30px', 'important');
-        }
         if(btn.classList.contains('active')){
             projectItems.forEach((item) => {
                 if(btn.dataset.type == item.dataset.proj){
@@ -137,7 +59,7 @@ burgerBtn.addEventListener('click', (e) => {
     headerMenu.classList.toggle('open');
     body[0].classList.toggle('fixing');
 });
-btnOrderOpenModal.addEventListener('click', (e) => {
+btnOrderOpenModal?.addEventListener('click', (e) => {
     e.preventDefault();
     modal.classList.add('active');
     modalOverlay.classList.add('overlay');
@@ -145,13 +67,13 @@ btnOrderOpenModal.addEventListener('click', (e) => {
         burgerBtn.setAttribute("disabled", "");
     }
 });
-btnCloseModal.addEventListener('click', (e) => {
+btnCloseModal?.addEventListener('click', (e) => {
     e.preventDefault();
     modal.classList.remove('active');
     modalOverlay.classList.remove('overlay');
     burgerBtn.removeAttribute("disabled", "");
 });
-modalOverlay.addEventListener('click', (e) => {
+modalOverlay?.addEventListener('click', (e) => {
     if(e.target == modalOverlay){
         modal.classList.remove('active');
         modalOverlay.classList.remove('overlay');
